634dc69faafeb0d56641c111a4149e27301b8a48
tamimkarim <67522154+tamimkarim@users.noreply.github.com>
2020-09-24 19:55:56 +0600

    problem solved

--- src/Components/CountryDetail/CountryDetail.js
+++ src/Components/CountryDetail/CountryDetail.js
@@ -4,20 +4,22 @@ import { useParams } from 'react-router-dom';
 const CountryDetail = () => {
     const {countryDetails} = useParams();
     const [country, setCountry] = useState({});
-    // const {name,capital,region,population,nativeName,flag } = country;
+    const {name,capital,region,population,nativeName,flag } = country;
 
     useEffect(() => {
         const url = `https://restcountries.eu/rest/v2/name/${countryDetails}`;
         fetch(url)
         .then(res => res.json())
-        .then(data => setCountry(data));
+        .then(data => setCountry(data[0]));
 
     }, [])
     return (
         <div>
             <h3>This is details of {countryDetails} </h3>
-            <p>This is : {countryDetails} </p>
-            <p>Capital : {country.alpha3Code} </p>
+            <h5>This is : {name} </h5>
+            <h6>Capital : {capital} </h6>
+
+
         </div>
     );
 };
